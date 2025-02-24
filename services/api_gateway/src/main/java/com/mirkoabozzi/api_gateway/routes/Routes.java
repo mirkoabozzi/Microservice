26b2d0a91ed06a8bd324ddb1bf40582bf53a1396
Mirko Abozzi <mirko.abozzi@gmail.com>
2025-02-24 15:38:58 +0100

    fixed routes

--- services/api_gateway/src/main/java/com/mirkoabozzi/api_gateway/routes/Routes.java
+++ services/api_gateway/src/main/java/com/mirkoabozzi/api_gateway/routes/Routes.java
@@ -1,5 +1,6 @@
 package com.mirkoabozzi.api_gateway.routes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
@@ -11,17 +12,22 @@ import org.springframework.web.servlet.function.ServerResponse;
 @Configuration
 public class Routes {
 
+    @Value("${product.service.uri}")
+    private String productServiceUri;
+    @Value("${order.service.uri}")
+    private String orderServiceUri;
+
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product_service")
-                .route(RequestPredicates.path("/api/products"), HandlerFunctions.http("http://localhost:8081"))
+                .route(RequestPredicates.path("/api/products/**"), HandlerFunctions.http(productServiceUri))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> orderServiceRoute() {
         return GatewayRouterFunctions.route("order_service")
-                .route(RequestPredicates.path("/api/orders"), HandlerFunctions.http("http://localhost:8082"))
+                .route(RequestPredicates.path("/api/orders/**"), HandlerFunctions.http(orderServiceUri))
                 .build();
     }
 }
